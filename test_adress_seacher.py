77b43f3e371bff7825fe1af8ea61597f1aeea85c
shotaro-ooe <shoooe97@gmail.com>
2018-11-24 17:29:32 +0900

    Move file

--- test_adress_seacher.py
+++ test_adress_seacher.py
@@ -1,34 +1,19 @@
 import unittest
-import requests
 
-
-class AdressSeacher(object):
-    def search(self, postal_code):
-        url = f"http://zipcloud.ibsnet.co.jp/api/search?zipcode=0287100 = {postal_code}"
-        response = requests.get(url)
-
-        response_dict = response.json()
-
-        都道府県 = response_dict["results"][0]["address1"]
-        市区町村 = response_dict["results"][0]["address2"]
-        町域 = response_dict["results"][0]["address3"]
-
-        return f"{都道府県}{市区町村}{町域}"
-
-
+from address_seacher import AddressSeacher
 
 
 class TestAddressSeacher(unittest.TestCase):
     def test_岩手県八幡平市大更の地名を郵便番号から取得できる(self):
-        address_seacher = AdressSeacher()
+        address_searcher = AddressSeacher()
 
-        actual = address_seacher.search(postal_code="0287111")
+        actual = address_searcher.search(postal_code="0287111")
         self.assertEqual("岩手県八幡平市大更", actual)
 
     def test_東京都練馬区豊玉南の地名を郵便番号から取得できる(self):
-        address_seacher = AdressSeacher()
+        address_searcher = AddressSeacher()
 
-        actual = address_seacher.search(postal_code="1760014")
+        actual = address_searcher.search(postal_code="1760014")
         self.assertEqual("東京都練馬区豊玉南", actual)
 
 
