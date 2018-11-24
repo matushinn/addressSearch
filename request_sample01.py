637f8b5065707ca344f84a9c6a5420e62d727831
shotaro-ooe <shoooe97@gmail.com>
2018-11-24 15:45:27 +0900

    api取得

---
+++ request_sample01.py
@@ -0,0 +1,8 @@
+import requests
+#1
+responce = requests.get(url="http://hachimantai.spartacamp.jp/")
+
+
+#2
+print(responce)
+
---
+++ request_sample02.py
@@ -0,0 +1,4 @@
+import requests
+
+responce = requests.get(url = "http://zipcloud.ibsnet.co.jp/api/search?zipcode=0287100")
+
